5448294f6543751daa5fa4b798b6f95fa3f62619
ArtemProskurin <67427391+ArtemProskurin@users.noreply.github.com>
2020-06-30 23:23:19 +0300

    Лабораторнай работа 10(а)

---
+++ Laboratory-work-10a/Lab10(a).cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	setlocale(LC_CTYPE, "");
+	int n,i,j,m,**a,sum=0;
+	printf("¬ведите кол-во строк в матрице:");
+	scanf("%d", &n);
+	printf("¬ведите кол-во столбцов в матрице:");
+	scanf("%d", &m);
+	a = new int *[n];
+	for (i = 0; i < n; i++)
+		a[i] = new int[m];
+	for (i = 0; i < n; i++) {
+		printf("¬ведите "); printf("%d", i + 1); printf("-ую строку массива:");
+		for (j = 0; j < m; j++)
+			scanf("%d", &a[i][j]);
+	}
+	printf("\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			printf("%5d", a[i][j]);
+		}
+		printf("\n");
+	}
+
+	_getch();
+}
